49b2f3d4a96462d9e9945a75a3e34cda35b7562f
Mahmuda Rahaman <rahaman2207032@stud.kuet.ac.bd>
2025-11-17 20:06:52 +0600

    Set width and height

--- src/main/java/com/example/cv_builder/HelloApplication.java
+++ src/main/java/com/example/cv_builder/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.cv_builder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,9 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setWidth(600);
+        stage.setHeight(600);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
