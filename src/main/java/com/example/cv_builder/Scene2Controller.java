771ad295defdd4b40506404c1c74278d98485ed8
Mahmuda Rahaman <rahaman2207032@stud.kuet.ac.bd>
2025-11-17 23:29:35 +0600

    Generated Alert message

--- src/main/java/com/example/cv_builder/Scene2Controller.java
+++ src/main/java/com/example/cv_builder/Scene2Controller.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -38,15 +39,24 @@ public class Scene2Controller {
     private ObservableList<CV_INFORMATION> CV_INFORMATION = FXCollections.observableArrayList();
     @FXML
     public void Add() {
-
+        CV_INFORMATION.add(new CV_INFORMATION(Name.getText(),Email.getText(), Phone.getText(), Address.getText(),Ed_Qualification.getText(),Skills.getText(),Work_experience.getText(),Projects.getText()));
     }
 
     public void SwitchScene3 (ActionEvent event) throws IOException {
+        Add();
+        if(isEmpty(Name)) return;
+        if(isEmpty(Email)) return;
+        if(isEmpty(Phone)) return;
+        if(isEmpty(Address)) return;
+        if(isEmpty(Skills)) return;
+        if(isEmpty(Work_experience)) return;
+        if(isEmpty(Projects)) return;
+        if(isEmpty(Ed_Qualification)) return;
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene3.fxml"));
         Parent root = (Parent) loader.load();
 
-        GenerateController controller = (GenerateController) loader.getController();
+        GenerateController controller = loader.getController();
 
         controller.loadData(CV_INFORMATION);
 
@@ -57,6 +67,17 @@ public class Scene2Controller {
         stage.show();
 
     }
+    private boolean isEmpty(TextField tf){
+        if(tf.getText().trim().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("Enter  all the required Informations");
+            alert.showAndWait();
+            return true;
+        }
+        return false;
+}
 
 
 }
