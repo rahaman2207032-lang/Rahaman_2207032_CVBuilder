0c3796768fa93118df53a5da69e303f193cc8692
Mahmuda Rahaman <rahaman2207032@stud.kuet.ac.bd>
2025-11-17 14:01:15 +0600

    All files changed

--- src/main/java/com/example/cv_builder/HelloController.java
+++ src/main/java/com/example/cv_builder/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.cv_builder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+   public void ClickButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setTitle("CV-Builder");
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
---
+++ src/main/java/com/example/cv_builder/Scene2Controller.java
@@ -0,0 +1,4 @@
+package com.example.cv_builder;
+
+public class Scene2Controller {
+}
